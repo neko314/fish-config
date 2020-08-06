d1c8818517372e01168c0924cf067942d8668159
Keiko Kaneko <keiko.cda@gmail.com>
2020-08-06 19:39:50 +0900

    Change ls color

--- config.fish
+++ config.fish
@@ -1,3 +1,6 @@
+## lsコマンド結果の配色
+export LSCOLORS=Gxfxcxdxbxegedabagacad
+
 ## rbenv init setting
 status --is-interactive; and source (rbenv init -|psub)
 set PATH  ~/.rbenv/shims:/usr/local/bin $PATH
