b35e5b49f51b2cbf7234c8905ddb1544f53ac887
Keiko Kaneko <keiko.cda@gmail.com>
2020-08-08 18:58:45 +0900

    Add setting

--- config.fish
+++ config.fish
@@ -6,4 +6,13 @@ status --is-interactive; and source (rbenv init -|psub)
 set PATH  ~/.rbenv/shims:/usr/local/bin $PATH
 
 ## npm setting
-set PAHT ~/.npm-global/bin $PATH
+set PAHT ~/.npm-global/bin $PATHset -g fish_user_paths "/usr/local/opt/icu4c/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/icu4c/sbin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/icu4c/bin" $fish_user_paths
+
+## pkg setting
+set -gx PKG_CONFIG_PATH "/usr/local/opt/icu4c/lib/pkgconfig"
+
+## compiler setting
+set -gx LDFLAGS "-L/usr/local/opt/icu4c/lib"
+set -gx CPPFLAGS "-I/usr/local/opt/icu4c/include" 
