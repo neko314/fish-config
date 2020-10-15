1aba92aa255e712ad4445dd62805d1e37a4d9f84
Keiko Kaneko <keiko.cda@gmail.com>
2020-10-15 14:46:24 +0900

    Add abbrs

--- config.fish
+++ config.fish
@@ -16,3 +16,19 @@ set -gx PKG_CONFIG_PATH "/usr/local/opt/icu4c/lib/pkgconfig"
 ## compiler setting
 set -gx LDFLAGS "-L/usr/local/opt/icu4c/lib"
 set -gx CPPFLAGS "-I/usr/local/opt/icu4c/include" 
+
+if status --is-interactive
+    abbr --add --global b     'bundle'
+    abbr --add --global bi    'bundle install'
+    abbr --add --global bx    'bundle exec'
+    abbr --add --global clean 'git branch --merged|egrep -v "\*|develop|master"|xargs git branch -d'
+    abbr --add --global ga    'git add'
+    abbr --add --global gb    'git branch'
+    abbr --add --global gc    'git commit -v'
+    abbr --add --global gco   'git checkout'
+    abbr --add --global gst   'git status'
+    abbr --add --global gl    'git log -p --stat'
+
+    # abbr --add --global graph 'log --pretty=format:'%Cgreen[%cd] %Cblue%h %Cred<%cn> %Creset%s' --date=short  --decorate --graph --branches --tags'
+    abbr --add --global pr    'gh pr checkout'
+end
