2ea050f33f396732a73bb6f6fac48599ea2b9359
Keiko Kaneko <keiko.cda@gmail.com>
2020-10-18 18:22:05 +0900

    Add commands to blueutil

--- config.fish
+++ config.fish
@@ -24,4 +24,19 @@ if status --is-interactive
   abbr --add --global gco 'git checkout'
   abbr --add --global gst 'git status'
   abbr --add --global gl  'git log --stat -p'
+
+  abbr --add --global btc 'blueutil --connect'
+  abbr --add --global btd 'blueutil --disconnect' 
+end
+
+# use blueutil
+# https://github.com/toy/blueutil
+function trackpad --wraps blueutil
+  if test -d $argv || test $argv = 'on'
+    blueutil --connect 14-c2-13-ee-fd-c1
+  else if test $argv = 'off'
+    blueutil --disconnect 14-c2-13-ee-fd-c1
+  else
+    echo wrong argument
+  end
 end
